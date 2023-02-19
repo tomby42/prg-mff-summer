83b7dec9a36dadd901e6b5378a4b7892311149a5
Tomas tomby Bily <tomby@ucw.cz>
2023-02-19 19:24:11 +0100

    prvni cviceni, readme

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# prg-mff-summer
+# NPRG031 Programování 2  – cvičení
+
+Podmínky zápočtu:
+- aktivní účast na cvičení
+- nejméně 70% bodů z domácích úkolů
+  - některé budou samostatné jiné párové
+  - párové musí odevzdat oba a to stejné řešení
+  - samostatné se musí lišit od ostatních řešení
+  - 50%-70% na konci semetru bude možno dodělat náročnější ůkoly během zk období
+- zápočtový test
+- zápočtový program (včetně dokumentace)
+  - projekt se odevzda jako gitove repo
+  - vypracování nejpozději do 15.9.2023!
+  - zadání tématu ke schválení nejpozději 1.6.2023
+
+Struktura adresaru:
+- src - zdrojove kody
+- README.md - this file
---
+++ src/15-02-2023.cs
@@ -0,0 +1,105 @@
+﻿/*
+https://www.youtube.com/watch?v=f84n5oFoZBc&list=PLZdCLR02grLrEwKaZv-5QbUzK0zGKOOcr&index=8
+
+*/
+
+/*
+mkdir MyApp
+cd MyApp
+dotnet new console
+
+File → Open Folder
+select the MyApp
+Select Yes 
+
+"console": "integratedTerminal" in launch.json file
+*/
+
+using System;
+
+namespace Cviceni {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Nazdar Svete");
+            
+            euclid();
+            horner();
+
+            int[] a = sort();
+            binsearch(a, int.Parse(Console.ReadLine()));
+        }
+
+        static void euclid() {
+            Console.WriteLine("Zadej dvě kladná celá čísla:");
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            while (a != b) {
+                if (a > b) {a -= b;}
+                else b -= a;
+            }
+            Console.WriteLine("Nejv. spol. dělitel: {0}", a);
+            Console.ReadLine(); 
+        }
+
+        static void horner() {
+            int v;
+            int c;
+            c = Console.Read();
+            // preskocit ne-cislice:
+            while ((c < '0') || (c > '9')) {
+                c = Console.Read();
+            }         
+            // nacitat cislice:
+            v = 0;
+            while ((c >= '0') && (c <= '9')) {
+                v = 10 * v + (c - '0');
+                c = Console.Read();
+            }
+            Console.WriteLine(v);
+            Console.ReadLine();  
+        }
+
+        static int[] sort() {
+            Console.Write("Počet čísel: ");
+            int pocet = int.Parse(Console.ReadLine());
+            int[] a;
+            a = new int[pocet];
+            int i = 0;
+            while (i < a.Length)
+                a[i++] = int.Parse(Console.ReadLine());
+            i = 0;
+            while (i < a.Length) {
+                int k = i;
+                int j = i+1;
+                while (j < a.Length) {
+                    if (a[j] < a[k]) k = j;
+                    j++;
+                }
+                if (k != i) {
+                    int x = a[i];
+                    a[i] = a[k];
+                    a[k] = x;
+                }
+                i++;
+            }
+            i = 0;
+            while (i < a.Length)
+                Console.Write(" {0}", a[i++]);
+            Console.WriteLine();
+
+            return a;  
+        }
+
+        static void binsearch(int[] a, int number) {
+            // Binarnim vyhledavanim najit index number
+        }
+
+        static int max(int[] a) {
+            return 0;
+        }
+
+        static void erastotenes() {
+
+        }
+    }
+}
